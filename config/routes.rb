b5d37d866ae384b76de858a7b880c0022fe8ef0e
ray lim <ziyang.lim89@gmail.com>
2021-09-22 13:36:33 +0800

    results page now print the restaurant cards with basic information

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # results routes
-  get "results", to: "restaurant#index"
+  get "results", to: "restaurants#index"
 end
