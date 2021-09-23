f21a1d164e803ab180be6697dcb052a25c3a9731
Ran Ci <ciran1990@protonmail.com>
2021-09-23 11:23:28 +0800

    not pushing

--- db/schema.rb
+++ db/schema.rb
@@ -62,16 +62,13 @@ ActiveRecord::Schema.define(version: 2021_09_22_093644) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "lists", "users"
   add_foreign_key "restaurant_lists", "lists"
   add_foreign_key "restaurant_lists", "restaurants"
   add_foreign_key "reviews", "restaurants"
   add_foreign_key "reviews", "users"
-
 end
