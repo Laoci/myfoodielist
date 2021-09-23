5e4ee8b21186dbcf261d5c405907f3f946e89a14
Audrey Kon <audz@kon.sg>
2021-09-23 16:25:00 +0800

    list controller made. Route for list added for happy flow. simple html for lists made

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,7 @@
 class List < ApplicationRecord
+  has_many :restaurants, through: :restaurant_lists
+  belongs_to :user
+
+  # Must have name, must be string, max 15 letters
+  validates :name, presence: true, length: { maximum: 15 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews
   end
+
+  # lists route
+  resources :lists
 end
